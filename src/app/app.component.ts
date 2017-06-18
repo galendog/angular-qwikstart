4f6bd663d0349537cf329a361d39630cfeccbe6f
Bob Landau <galen_dog@msn.com>
2017-06-18 15:14:30 -0400

    Adding support for editing the Hero characters
    This check0-in adds support for one-way data-binding via the
    {{object.property}} syntax as well as two-way data-binding using the [(n
    gModel)] directive.
    
    This also using template literals using back-ticks ` to allow multiline
    strings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,34 @@
 import { Component } from '@angular/core';
 
+
+// double curly braces {{object.property}} (one-way data binding) are Angular's interpolation binding syntax
+// [(ngModel)] directive is the Angular syntax for two-way data binding
+// The backticks designate a template literal a feature of ES2015 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+     <h1>{{title}}</h1>
+	 <h2>{{hero.name}} details</h2>
+	 <div><label>id: </label>{{hero.id}}</div>
+	 <div>
+	    <label>name: </label>
+		<input [(ngModel)]="hero.name" placeholder="name">
+     </div>
+	 `
 })
-export class AppComponent  { name = 'Angular'; }
+
+
+export class AppComponent
+{ 
+   name = 'Angular'; 
+   title = 'Tour of Heros';
+   hero = Hero = {
+	   id: 1,
+	   name: 'Windstorm'
+   };
+}
+
+export class Hero {
+	id: number;
+	name: string;
+}
