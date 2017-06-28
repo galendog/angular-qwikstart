8e7e99d5f61717aee6c4559c9dd9487b67d41266
galendog <galen_dog@msn.com>
2017-06-28 08:56:52 -0700

    Final phase in the Routing Tutorial of the 'Tour of Heroes'
    Moved the last of the HTML and CSS code out of the typescript files and
    into their respective HTML and CSS files.
    
    Add support for viewing the HeroDetails when the 'View Details' button
    is clicked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,9 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
    selector: 'my-app',
-   template: `
-      <h1>{{title}}</h1>
-      <nav>
-         <a routerLink="/dashboard">Dashboard</a>
-         <a routerLink="/heroes">Heroes</a>
-      </nav>
-      <router-outlet></router-outlet>
-`
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
